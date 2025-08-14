96ed06eeb191811cd889567ca2b05208cad085f1
AmandaCornelsen <cornelsen089@gmail.com>
2025-08-13 21:42:48 -0300

    Teste com usuarios em lista e try/catch

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,33 @@
     const express = require('express')
     const router = new express.Router();
 
+    let users = [
+        {id:1, nome: "Amanda Cornelsen", idade: 20},
+        {id:2, nome: "Vinicius Shuet", idade: 20}
+    ]
+    
     //endpoint
     router.get('/', (req, res, next) => {
-        res.status(200).send({
-            "nome" : "Amanda Cornelsen"
-        });
+        
+        try {
+            res.status(200).send(
+                {
+                    success: true,
+                    message: "Usuários encontrados",
+                    data: users,
+                    total: users.length
+                }
+            );
+        }catch(error){
+            //Status code 500 - indica erro interno
+            res.send(500).json(
+                {
+                    success: false,
+                    message: "Erro interno",
+                    error: error.message
+                }
+            )
+        }     
     });
 
     module.exports = router;
